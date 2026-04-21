c08b24226909a60bea6990974bd1595baf347589
gustavo <gs8q124gl@gmail.com>
2026-04-20 21:57:25 -0300

    Criação do endpoint para listar usuários e relacionamento com tarefas

--- backend/tarefas/models.py
+++ backend/tarefas/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from usuarios.models import Usuario
 
-# Create your models here.
+class Tarefa(models.Model):
+    STATUS_CHOICES = [
+        ('ABERTA', 'Aberta'),
+        ('EM_ANDAMENTO', 'Em andamento'),
+        ('CONCLUIDA', 'Concluída'),
+        ('CANCELADA', 'Cancelada'),
+    ]
+
+    PRIORIDADE_CHOICES = [
+        ('URGENTE', 'Urgente'),
+        ('NAO_URGENTE', 'Não Urgente'),
+    ]
+
+    from django.db import models
+from django.utils import timezone # Adicione esta importação
+from usuarios.models import Usuario
+
+class Tarefa(models.Model):
+    STATUS_CHOICES = [
+        ('ABERTA', 'Aberta'),
+        ('EM_ANDAMENTO', 'Em andamento'),
+        ('CONCLUIDA', 'Concluída'),
+        ('CANCELADA', 'Cancelada'),
+    ]
+
+    PRIORIDADE_CHOICES = [
+        ('URGENTE', 'Urgente'),
+        ('NAO_URGENTE', 'Não Urgente'),
+    ]
+
+    
+    titulo = models.CharField(max_length=255, default='')
+    descricao = models.TextField(default='')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='ABERTA')
+    prioridade = models.CharField(max_length=15, choices=PRIORIDADE_CHOICES, default='NAO_URGENTE')
+    data_criacao = models.DateTimeField(default=timezone.now) 
+    data_entrega = models.DateField(default=timezone.now)
+    
+    usuario_responsavel = models.ForeignKey(
+        Usuario, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True
+    )
+
+    def __str__(self):
+        return self.titulo
--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Tarefa
 
-# Create your views here.
+def listar_tarefas(request):
+    tarefas = Tarefa.objects.all().values()
+    return JsonResponse(list[any](tarefas), safe=False)
+
+def listar_tarefas_abertas(request):
+    tarefas_abertas = Tarefa.objects.filter(status='ABERTA').values()
+    return JsonResponse(list(tarefas_abertas), safe=False)   
